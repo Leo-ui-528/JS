21abf9f28018a0df3609ef66eb8dbd2606e2ca31
Leo-ui-528 <75322878+Leo-ui-528@users.noreply.github.com>
2021-08-24 18:48:53 +0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,3 +1,43 @@
-var tc = 123;
-var tf = 1.8 * tc + 32;
-console.log(tf);
+var arr = [];
+var n = 1;
+var a = 2;
+var b = 2;
+while (n < 100) {
+    n++;
+    arr.push(n);
+}
+while (a <= 100) {
+    a += b;
+    arr.splice(arr.indexOf(a), 1);
+}
+
+a = 4;
+b = 3;
+while (a < 100) {
+    if (a % b === 0 && arr.indexOf(a) !== -1) {
+        arr.splice(arr.indexOf(a), 1);
+    } else {
+        a++;
+    }
+}
+a = 6;
+b = 5;
+while (a < 100) {
+    if (a % b === 0 && arr.indexOf(a) !== -1) {
+        arr.splice(arr.indexOf(a), 1);
+    } else {
+        a++;
+    }
+}
+
+a = 8;
+b = 7;
+while (a < 100) {
+    if (a % b === 0 && arr.indexOf(a) !== -1) {
+        arr.splice(arr.indexOf(a), 1);
+    } else {
+        a++;
+    }
+}
+
+console.log(arr);
--- 2.js
+++ 2.js
@@ -1,3 +1,9 @@
-var name = 'Василий';
-var admin = (name);
-console.log(name);
+var arr = new Array(6, 7, 11, 2, 4, 6, 45);
+var sum = 0;
+function countBasketPrice(arra) {
+    for (var i = 0; i < arr.length; i++) {
+        sum = sum + parseInt(arr[i]);
+    }
+    console.log(sum);
+}
+countBasketPrice(arr)
